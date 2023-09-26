a178b3b679c9bcc13fa4a3187f80d7c5feb41773
Douglas Hellberg <DougHellberg@gmail.com>
2023-09-26 17:32:24 -0400

    Adding Bailey's code to the repo

--- lib/main.py
+++ lib/main.py
@@ -1,4 +1,121 @@
 import os;
+import tkinter as tk
+import sqlite3
+
+
+# Create a database connection and cursor
+CONN = sqlite3.connect('user_accounts.db')
+CURSOR = CONN.cursor()
+
+# Create a table for user accounts if it doesn't exist
+CURSOR.execute('''CREATE TABLE IF NOT EXISTS accounts (
+                id INTEGER PRIMARY KEY,
+                username TEXT,
+                password TEXT
+            )''')
+CONN.commit()
+
+# Function to create a user account and open the clothing item addition interface
+def create_account():
+    username = username_entry.get()
+    password = password_entry.get()
+
+    if username and password:
+        CURSOR.execute("INSERT INTO accounts (username, password) VALUES (?, ?)",
+                    (username, password))
+        CONN.commit()
+        status_label.config(text="Account created successfully")
+
+        # Close the account creation window
+        root.destroy()
+
+        # Open the clothing item addition window
+        open_clothing_window()
+    else:
+        status_label.config(text="Please fill in both username and password fields")
+
+# Function to open the clothing item addition interface
+def open_clothing_window():
+    # Create a new window for adding clothing items
+    clothing_root = tk.Tk()
+    clothing_root.title("Clothing Inventory")
+
+    # Rest of the code for the clothing item addition interface (similar to the previous example)
+
+    # Start the clothing item addition GUI application
+    clothing_root.mainloop()
+
+# Create the main window for account creation
+root = tk.Tk()
+root.title("Account Creation")
+
+root.mainloop()
+
+CONN = sqlite3.connect('clothing.db')
+CURSOR = CONN.cursor()
+
+# Create a table for clothing items if it doesn't exist
+CURSOR.execute('''CREATE TABLE IF NOT EXISTS clothing (
+                id INTEGER PRIMARY KEY,
+                item_name TEXT,
+                category TEXT,
+                color TEXT,
+                size TEXT
+            )''')
+CONN.commit()
+
+# Function to add a clothing item to the database
+def add_clothing():
+    item_name = item_name_entry.get()
+    category = category_entry.get()
+    color = color_entry.get()
+    size = size_entry.get()
+
+    if item_name and category and color and size:
+        CURSOR.execute("INSERT INTO clothing (item_name, category, color, size) VALUES (?, ?, ?, ?)",
+                        (item_name, category, color, size))
+        CONN.commit()
+        status_label.config(text="Clothing item added successfully")
+    else:
+        status_label.config(text="Please fill in all fields")
+
+# Create the main window
+root = tk.Tk()
+root.title("Clothing Inventory")
+
+# Labels and Entry widgets for input
+item_name_label = tk.Label(root, text="Item Name:")
+item_name_label.pack()
+item_name_entry = tk.Entry(root)
+item_name_entry.pack()
+
+category_label = tk.Label(root, text="Category:")
+category_label.pack()
+category_entry = tk.Entry(root)
+category_entry.pack()
+
+color_label = tk.Label(root, text="Color:")
+color_label.pack()
+color_entry = tk.Entry(root)
+color_entry.pack()
+
+size_label = tk.Label(root, text="Size:")
+size_label.pack()
+size_entry = tk.Entry(root)
+size_entry.pack()
+
+# Button to add clothing item
+add_button = tk.Button(root, text="Add Clothing Item", command=add_clothing)
+add_button.pack()
+
+# Status label to display messages
+status_label = tk.Label(root, text="")
+status_label.pack()
+
+# Start the GUI application
+root.mainloop()
+
+
 
 def menu ():
     os.system('cls||clear')
